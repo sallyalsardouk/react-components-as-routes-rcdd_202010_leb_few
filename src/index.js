7295a0cc681f9d3896afa5a8d00431e0b1f102fa
sally al sardouk <sally.sardouk98@gmail.com>
2021-02-12 08:59:52 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route ,NavLink} from 'react-router-dom';
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+ 
 
 const Home = () => {
   return (
@@ -8,8 +18,39 @@ const Home = () => {
     </div>
   );
 };
+const About = () => {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
+};
+ 
+const Login = () => {
+  return (
+    <div>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+          <label htmlFor="username">Username</label>
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+          <label htmlFor="password">Password</label>
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
+};
 
 ReactDOM.render(
-  <Home />,
+  <Router>
+  <div>
+    <Route path="/" component={Home} />
+    <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+      </div>
+  </Router>),
   document.getElementById('root')
 );
